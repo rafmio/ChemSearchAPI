a145f2c11b2de8105afb63ca8da68f83089f6a05
Rafael Idrisov <raf@localhost.localdomain>
2025-09-24 14:39:43 +0300

    Work PC 24.09.2025 14:39

--- setup_env.sh
+++ setup_env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export YANDEX_WORDSTAT_CLIENT_ID="fbd1eed6387846c284af560bdb8d33ea"
-export YANDEX_WORDSTAT_CLIENT_SECRET="4844e5bfb8b24713914b17788764b0a1"
+export YANDEX_WORDSTAT_CLIENT_SECRET="c3f3dfb8a0c64e9c97af1c00ed706e7e"
 
 # Запускаем программу go
 go run .
